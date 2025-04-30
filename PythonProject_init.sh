ae17ce1250b27012eed9d3f954ecd63640d0ca73
emix1984 <60463282+emix1984@users.noreply.github.com>
2025-04-30 23:32:41 +0900

    Update PythonProject_init.sh
    优化代码

--- PythonProject_init.sh
+++ PythonProject_init.sh
@@ -8,22 +8,22 @@ install_dependencies() {
     libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget
 }
 
-# 定义函数：安装 Python 3.10
-install_python310() {
-    echo "正在安装 Python 3.10..."
-    apt install -y python3.10 python3.10-venv python3.10-pip
+# 定义函数：安装 Python 3 及相关软件包
+install_python3() {
+    echo "正在安装 Python 3 及相关软件包..."
+    apt install -y python3 python3-pip python3-venv
 }
 
 # 定义函数：更新 pip 到最新版本
 update_pip() {
     echo "正在更新 pip 到最新版本..."
-    python3.10 -m pip install --upgrade pip
+    python3 -m pip install --upgrade pip
 }
 
 # 定义函数：创建虚拟环境并安装依赖
 setup_venv_and_install_requirements() {
     echo "正在创建虚拟环境..."
-    python3.10 -m venv ./venv
+    python3 -m venv ./venv
 
     echo "正在激活虚拟环境..."
     source ./venv/bin/activate
@@ -42,13 +42,14 @@ setup_venv_and_install_requirements() {
 # 主程序
 echo "开始设置 Python 环境..."
 
-# 检查是否已安装 Python 3.10
-if ! command -v python3.10 &> /dev/null; then
-    echo "Python 3.10 未安装，正在安装必要依赖和 Python 3.10..."
-    install_dependencies
-    install_python310
+# 检查是否已安装 Python 3
+if command -v python3 &> /dev/null; then
+    echo "Python 3 已安装。"
+    echo "Python 3 的路径为：$(which python3)"
 else
-    echo "Python 3.10 已安装。"
+    echo "Python 3 未安装，正在安装必要依赖和 Python 3..."
+    install_dependencies
+    install_python3
 fi
 
 # 更新系统中的 pip 到最新版本
